6af90a05e1d1c87d621ff0cd2241d5980e5e1f75
Lee, Chun-Yi <jlee@suse.com>
2017-10-30 14:26:18 +0800

    moktest: remove cert because moktest includes

--- mok-kernel-module-testing/cert/mok-test-kmp-dump-cert.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-osc signkey --sslcert > uefi-plugfest.pem
-openssl x509 -inform PEM -in uefi-plugfest.pem -outform DER -out uefi-plugfest.der
-openssl x509 -in uefi-plugfest.der -inform DER -noout -text
